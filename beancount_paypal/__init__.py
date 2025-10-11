e6b5c2e813b3bf934a024f71493786874c8d59b0
Omar Kohl <omarkohl@posteo.net>
2025-10-11 10:37:15 +0200

    style: lint and format code with ruff

--- beancount_paypal/__init__.py
+++ beancount_paypal/__init__.py
@@ -1,22 +1,20 @@
 from beancount.core.number import D
 from beancount.ingest import importer
-from beancount.core import account
 from beancount.core import amount
 from beancount.core import flags
 from beancount.core import data
 
-from dateutil.parser import parse
-from datetime import datetime, timedelta
+from datetime import timedelta
 from contextlib import contextmanager
 
 import csv
-import os
 
 from . import lang
 
+
 @contextmanager
 def csv_open(filename):
-    with open(filename, newline='', encoding='utf-8-sig') as f:
+    with open(filename, newline="", encoding="utf-8-sig") as f:
         yield csv.DictReader(f, quotechar='"')
 
 
@@ -28,7 +26,7 @@ class PaypalImporter(importer.ImporterProtocol):
         checking_account,
         commission_account,
         language=None,
-        metadata_map=None
+        metadata_map=None,
     ):
         if language is None:
             language = lang.en()
@@ -54,13 +52,14 @@ class PaypalImporter(importer.ImporterProtocol):
                     return False
 
                 row = self.language.normalize_keys(row)
-                if not (row['from'] == self.email_address or row['to'] == self.email_address):
+                if not (
+                    row["from"] == self.email_address or row["to"] == self.email_address
+                ):
                     return False
 
                 return True
             except StopIteration:
                 return False
-
 
     def extract(self, filename):
         entries = []
@@ -71,104 +70,125 @@ class PaypalImporter(importer.ImporterProtocol):
 
         with csv_open(filename.name) as rows:
             for index, row in enumerate(rows):
-                metadata = { k: row[v] for k, v in self.metadata_map.items() }
+                metadata = {k: row[v] for k, v in self.metadata_map.items()}
                 row = self.language.normalize_keys(row)
 
-                row['date'] = self.language.parse_date(row['date']).date()
-                row['gross'] = self.language.decimal(row['gross'])
-                row['fee'] = self.language.decimal(row['fee'])
-                row['net'] = self.language.decimal(row['net'])
+                row["date"] = self.language.parse_date(row["date"]).date()
+                row["gross"] = self.language.decimal(row["gross"])
+                row["fee"] = self.language.decimal(row["fee"])
+                row["net"] = self.language.decimal(row["net"])
 
-                if row['reference_txn_id'] != last_txn_id:
+                if row["reference_txn_id"] != last_txn_id:
                     meta = data.new_metadata(filename.name, index, metadata)
 
                     txn = data.Transaction(
                         meta=meta,
-                        date=row['date'],
+                        date=row["date"],
                         flag=flags.FLAG_OKAY,
-                        payee=row['name'],
-                        narration=row.get('item_title') or row.get('subject') or row.get('note'),
+                        payee=row["name"],
+                        narration=row.get("item_title")
+                        or row.get("subject")
+                        or row.get("note"),
                         tags=set(),
                         links=set(),
                         postings=[],
                     )
 
-                if self.language.txn_from_checking(row['txn_type']):
+                if self.language.txn_from_checking(row["txn_type"]):
                     txn.postings.append(
                         data.Posting(
                             self.checking_account,
-                            amount.Amount(-1*D(row['gross']), row['currency']),
-                            None, None, None, None
+                            amount.Amount(-1 * D(row["gross"]), row["currency"]),
+                            None,
+                            None,
+                            None,
+                            None,
                         )
                     )
 
                     txn.postings.append(
                         data.Posting(
                             self.account,
-                            amount.Amount(D(row['net']), row['currency']),
-                            None, None, None, None
+                            amount.Amount(D(row["net"]), row["currency"]),
+                            None,
+                            None,
+                            None,
+                            None,
                         )
                     )
 
-                elif self.language.txn_currency_conversion(row['txn_type']):
+                elif self.language.txn_currency_conversion(row["txn_type"]):
                     if last_was_currency:
                         txn.postings.append(
                             data.Posting(
                                 self.account,
                                 amount.Amount(D(last_net), last_currency),
-                                None, None, None, None
+                                None,
+                                None,
+                                None,
+                                None,
                             )
                         )
                         txn.postings.append(
                             data.Posting(
                                 self.account,
-                                amount.Amount(D(row['net']), row['currency']),
+                                amount.Amount(D(row["net"]), row["currency"]),
                                 None,
-                                amount.Amount(-1*(D(last_net) / D(row['net'])), last_currency),
-                                None, None
+                                amount.Amount(
+                                    -1 * (D(last_net) / D(row["net"])), last_currency
+                                ),
+                                None,
+                                None,
                             )
                         )
                         last_net = None
                         last_currency = None
                         last_was_currency = False
                     else:
-                        last_net = row['net']
-                        last_currency = row['currency']
+                        last_net = row["net"]
+                        last_currency = row["currency"]
                         last_was_currency = True
 
                 else:
                     txn.postings.append(
                         data.Posting(
                             self.account,
-                            amount.Amount(D(row['gross']), row['currency']),
-                            None, None, None, None
+                            amount.Amount(D(row["gross"]), row["currency"]),
+                            None,
+                            None,
+                            None,
+                            None,
                         )
                     )
 
-                if D(row['fee']) > 0:
+                if D(row["fee"]) > 0:
                     txn.postings.append(
                         data.Posting(
                             self.commission_account,
-                            amount.Amount(D(row['fee']), row['currency']),
-                            None, None, None, None
+                            amount.Amount(D(row["fee"]), row["currency"]),
+                            None,
+                            None,
+                            None,
+                            None,
                         )
                     )
 
-                if row['reference_txn_id'] != last_txn_id:
+                if row["reference_txn_id"] != last_txn_id:
                     entries.append(txn)
-                    last_txn_id = row['txn_id']
+                    last_txn_id = row["txn_id"]
 
-                last_currency = row['currency']
-                last_amount = amount
+                last_currency = row["currency"]
 
-        if 'balance' in row:
+        if "balance" in row:
             meta = data.new_metadata(filename.name, index + 1)
             entries.append(
                 data.Balance(
                     meta,
-                    row['date'] + timedelta(days=1),
+                    row["date"] + timedelta(days=1),
                     self.account,
-                    amount.Amount(D(self.language.decimal(row['balance'])), row['currency']),
+                    amount.Amount(
+                        D(self.language.decimal(row["balance"])), row["currency"]
+                    ),
                     None,
                     None,
                 )
